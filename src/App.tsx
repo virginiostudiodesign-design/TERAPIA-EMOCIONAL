9766d536677c73a9bb38124ed3b68212ac90bb08
virginiostudiodesign-design <virginio.studio.design@gmail.com>
2026-05-17 21:32:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,68 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
 
-import MentalBlocksDashboard from './components/MentalBlocksDashboard';
+import { useState } from 'react'
+import { supabase } from './supabase'
 
-export default function App() {
+function App() {
+
+  const [nome, setNome] = useState('')
+  const [idade, setIdade] = useState('')
+  const [medo, setMedo] = useState('')
+
+  async function salvarAnamnese() {
+
+    const { error } = await supabase
+      .from('anamnese')
+      .insert([
+        {
+          nome,
+          idade,
+          medo
+        }
+      ])
+
+    if (error) {
+      alert('Erro ao salvar')
+      console.log(error)
+    } else {
+      alert('Anamnese salva com sucesso')
+    }
+  }
+
   return (
-    <MentalBlocksDashboard />
-  );
+    <div style={{ padding: 40 }}>
+
+      <h1>Painel Emocional</h1>
+
+      <input
+        placeholder="Nome"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        placeholder="Idade"
+        value={idade}
+        onChange={(e) => setIdade(e.target.value)}
+      />
+
+      <br /><br />
+
+      <textarea
+        placeholder="Maior medo"
+        value={medo}
+        onChange={(e) => setMedo(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={salvarAnamnese}>
+        Salvar
+      </button>
+
+    </div>
+  )
 }
 
+export default App
